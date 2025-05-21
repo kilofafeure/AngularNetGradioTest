d43e31017a7962bae8f726efb2b6bf6c2d829d0f
m.gonzalez.bitrian@changetheblock.com <m.gonzalez.bitrian@changetheblock.com>
2025-05-21 10:18:02 +0200

    creando menu

--- AngularNetGradioTest/angularnetgradiotest.client/src/app/components/header/header.component.ts
+++ AngularNetGradioTest/angularnetgradiotest.client/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router"
 import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
@@ -6,8 +7,13 @@ import { AuthService } from '../../services/auth/auth.service';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   standalone: true,
+  imports: [RouterLink, RouterLinkActive],
 })
 export class HeaderComponent {
+  menuItems = [
+    { path: "/weatherforecast", label: "Weather F." },
+  ]
+
   constructor(private authService: AuthService) { }
 
   logout() {
